cd07742eb5d03741fe64583e4b72f0e1aa9daa3d
platipietro-web <platipietro-web@users.noreply.github.com>
2026-05-04 16:01:30 +0000

    deploy: 3d16605d1d4dd20a16bcde2019d1e11e5aa57a59

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2299098946" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1581151905" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
